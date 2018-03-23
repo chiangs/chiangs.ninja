6ef0a4e475726200e561b1146ef1c0ebce11b246
chiangs <stephen.e.chiang@gmail.com>
2018-03-23 22:54:11 +0100

    router added, services and models stubbed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { AppRouting } from './app-routing.module';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LandingComponent, NavbarComponent],
+  imports: [BrowserModule, AppRouting],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
