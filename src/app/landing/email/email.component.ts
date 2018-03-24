5153bb31c8bbebf0461d6f5af63c5eaab23d4cf2
chiangs <stephen.e.chiang@gmail.com>
2018-03-24 22:44:44 +0100

    slider animation 50% working

--- src/app/landing/email/email.component.ts
+++ src/app/landing/email/email.component.ts
@@ -4,8 +4,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-email',
   template: `
-  <div class="ui right labeled big input">
-    <label for="email" class="ui label">contact me</label>
+  <div class="ui right labeled big input" >
+    <label *ngIf="!submitted" for="email" class="ui label">contact me</label>
+    <label *ngIf="submitted" for="email" class="ui label" id="slider">thanks!</label>
     <input placeholder="email@domain" id="email" type="email">
     <div class="ui label" id="emailSubmit" (click)="submit()">
       <p *ngIf="!submitted" class="submitText">submit</p>
@@ -24,11 +25,22 @@ export class EmailComponent implements OnInit {
     this.submitted = false;
   }
 
+  slide(): void {
+    const slider = document.getElementById('slider');
+    slider.classList.add('submitted');
+  }
+
   // click starts animation, then routes to about
   submit() {
     this.submitted = true;
     setTimeout(() => {
-      this.router.navigate(['about']);
-    }, 1500);
+      this.slide();
+    }, 500);
+    // setTimeout(() => {
+    //   this.router.navigate(['about']);
+    // }, 1500);
   }
+
+  // TODO: Disable on invalid
+  // TODO: try animated version of each part of the input instead and stage them 0 - 100%;
 }
