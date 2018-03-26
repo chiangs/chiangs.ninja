31bc4e9c866d3ec099df1b44c9590c5d00e82e29
chiangs <stephen.e.chiang@gmail.com>
2018-03-26 20:13:20 +0200

    navbar skeleton and menus, colors, active class

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,17 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: `
-    <p>
-      navbar works!
-    </p>
+  <nav id="navbar">
+    <ul class="navList">
+        <li class="brand">
+          <a [routerLink]="['/welcome']">Brand</a>
+        </li>
+        <li *ngFor="let item of menu">
+            <a [routerLink]="[item.url]" 
+            routerLinkActive="active"
+            routerLinkActiveOptions="{exact: true}">{{ item.name }}</a>
+        </li>
+        <li id="socialMenu">@follow</li>
+        <li id="blog">
+          <a [routerLink]="['/blog']">blog</a>
+        </li>
+    </ul>
+  </nav>
   `,
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  menu: { name: string; url: string }[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.menu = [
+      { name: 'home', url: '/about' },
+      { name: 'projects', url: '/projects' },
+      { name: 'design', url: '/design' },
+      { name: 'photography', url: '/photography' },
+      { name: 'muay thai', url: '/muaythai' }
+    ];
   }
 
+  ngOnInit() {}
 }
